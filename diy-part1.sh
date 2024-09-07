038ba82d922039070900e7620d582a43b53734c0
Supremeyyds <95127349+Supremeyyds@users.noreply.github.com>
2024-09-07 17:07:26 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,11 @@
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/TWRT/g' package/base-files/files/bin/config_generate
+
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='TWRT '/g" package/lean/default-settings/files/zzz-default-settings
+
+wget https://raw.githubusercontent.com/Supremeyyds/Actions-OpenWrt/main/add_h28k_support.patch
+git apply add_h28k_support.patch
+rm package/feeds/luci/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-serverchan
